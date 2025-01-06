9042cfa5c7cbb721acd3f53ff4a219cb31029980
huyle74 <legiahuy74@gmail.com>
2025-01-06 12:20:21 +0700

    change footer

--- src/app/components/main-footer.tsx
+++ src/app/components/main-footer.tsx
@@ -1,161 +1,157 @@
 import { useRef, useEffect, useState } from "react";
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, useMediaQuery, Grid2 } from "@mui/material";
 import PhoneIphoneOutlinedIcon from "@mui/icons-material/PhoneIphoneOutlined";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import gsap from "gsap";
 
-type ChildProps = {
-  icon: React.ReactNode;
-  contact: string;
-};
-
-const Contact: React.FC<ChildProps> = ({ icon, contact }) => {
-  const infoRef = useRef<HTMLDivElement | null>(null),
-    iconRef = useRef<HTMLDivElement | null>(null),
-    [shake, setShake] = useState<boolean>(false),
-    matches = useMediaQuery("(max-width:600px)");
-
-  useEffect(() => {
-    let animation;
-    if (shake) {
-      animation = gsap.to([infoRef.current, iconRef.current], {
-        rotate: 1,
-        repeat: -1,
-        yoyo: true,
-        duration: 0.2,
-        ease: "back.in",
-      });
-    } else {
-      animation = gsap.to([infoRef.current, iconRef.current], {
-        rotate: 0,
-        repeat: 0,
-        yoyo: false,
-        duration: 0,
-      });
-    }
-    return () => {
-      animation.kill();
-    };
-  }, [shake]);
-
-  const handleEnter = () => {
-    if (!shake) {
-      setShake(true);
-    }
-  };
-
-  const handleLeave = () => {
-    if (shake) {
-      setShake(false);
-    }
-  };
+export default function MainFooter() {
+  const matches = useMediaQuery("(max-width:758px)");
 
   return (
     <Box
       sx={{
-        mt: 3,
-        width: "100%",
+        height: `${matches ? "100%" : "100vh"}`,
+        color: "white",
         position: "relative",
+        backgroundSize: "cover",
+        backgroundPosition: "50% 70%",
+        backgroundImage:
+          'url("https://storage.cloud.google.com/luyen-wedding-academy-bucket/class-image.jpg")',
+        fontFamily: "arial",
       }}
-      onMouseEnter={handleEnter}
-      onMouseLeave={handleLeave}
-      className="contact-info"
+      id="contact"
     >
-      <img
-        style={{ objectFit: "cover", width: "100%", height: "auto" }}
-        src="https://storage.cloud.google.com/luyen-wedding-academy-bucket/pink-background.jpg"
-        alt=""
-      />
-      <a
-        href="#"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
+      <Box
+        sx={{
           position: "absolute",
-          top: "40%",
-          left: "50%",
-          transform: "translate(-60%, -50%)",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+        }}
+      ></Box>
+      <Box
+        sx={{
+          zIndex: 99,
+          position: "relative",
+          textAlign: "center",
+          height: "50%",
+          alignContent: "center",
         }}
       >
-        <Box ref={iconRef}>{icon}</Box>
-        <Box
-          ref={infoRef}
-          sx={{
-            fontSize: `${matches ? "0.9rem" : "1.3rem"}`,
-            mt: 2,
-            fontFamily: "arial",
-            fontWeight: 600,
+        <h2
+          style={{
+            width: "50%",
+            margin: "auto",
+            border: "white 1px solid",
+            padding: "30px",
           }}
         >
-          {contact}
-        </Box>
-      </a>
-    </Box>
-  );
-};
-
-export default function MainFooter() {
-  const matches = useMediaQuery("(max-width:600px)");
-
-  return (
-    <div>
+          THAM GIA LUYẾN ACADEMY ĐỂ TRỞ BIẾN Ý TƯỞNG CỦA BẠN THÀNH 1 CHIẾC VÁY
+          ƯỚC MƠ
+        </h2>
+      </Box>
       <Box
-        className="footer-container"
-        sx={{ height: `${matches ? "100%" : "100vh"}` }}
-        id="contact"
+        sx={{
+          height: "50%",
+          backgroundColor: "#2d2f31",
+          clipPath: "polygon(0 16%, 100% 0%, 100% 100%, 0% 100%)",
+          display: "flex",
+        }}
       >
         <Box
           sx={{
+            width: `${matches ? "100%" : "50%"}`,
+            m: "auto",
+            // border: "1px white solid",
             display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-        >
-          <h1
-            style={{
-              textAlign: "center",
-              fontFamily: "arial",
-              fontSize: "3rem",
-            }}
-          >
-            CONTACT
-          </h1>
-          <img
-            style={{ transform: "rotate(-30deg)" }}
-            src="https://png.pngtree.com/png-clipart/20220109/original/pngtree-wedding-dress-design-png-image_7032599.png"
-            alt="image"
-          />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
             flexDirection: `${matches ? "column" : "row"}`,
+            justifyContent: "space-between",
           }}
         >
-          <Contact
-            icon={
-              <PhoneIphoneOutlinedIcon
-                sx={{ fontSize: "2rem", color: "white" }}
+          <Grid2 container>
+            <Grid2
+              size={{ md: 4, xs: 12 }}
+              sx={{
+                color: "white",
+                flexDirection: "column",
+              }}
+            >
+              <img
+                src="https://storage.cloud.google.com/luyen-wedding-academy-bucket/logo.jpg"
+                alt="logo"
+                style={{ objectFit: "cover", width: "35%", height: "auto" }}
               />
-            }
-            contact={"(+84) 93 196 90 91"}
-          />
-          <Contact
-            icon={<MailOutlineIcon sx={{ fontSize: "2rem", color: "white" }} />}
-            contact={"info@luyenacademy.com"}
-          />
-        </Box>
-        <Box sx={{ fontFamily: "arial", p: 3 }}>
-          <h1>ĐỊA CHỈ</h1>
-          <p style={{ marginTop: "1rem" }}>
-            410 Đ. Nguyễn Văn Tạo, Long Thới, Nhà Bè, Hồ Chí Minh
-          </p>
+              <h3
+                style={{
+                  backgroundColor: "#f01267",
+                  width: "min-content",
+                  marginTop: "10px",
+                }}
+              >
+                LUYẾN ACADEMY
+              </h3>
+              <p
+                style={{
+                  fontWeight: 100,
+                  fontSize: "0.9rem",
+                  marginTop: "15px",
+                  width: "50%",
+                }}
+              >
+                410 Đ. Nguyễn Văn Tạo, Long Thới, Nhà Bè, Hồ Chí Minh
+              </p>
+              <a
+                href="#"
+                style={{
+                  fontWeight: 1000,
+                  fontSize: "0.8rem",
+                  marginTop: "20px",
+                }}
+              >
+                (+84) 93 196 90 91
+              </a>
+            </Grid2>
+            <Grid2 id="footer-sitemap" size={{ md: 4, xs: 12 }}>
+              <h2>MENU</h2>
+              <a href="#">Khóa học</a>
+              <a href="#">Sản phẩm</a>
+              <a href="#">Gia công váy đầm</a>
+              <a href="#">Liên Hệ</a>
+            </Grid2>
+            <Grid2 className="social-network-icon" size={{ md: 4, xs: 12 }}>
+              <a href="#" target="blank">
+                <img
+                  style={{
+                    width: "80px",
+                  }}
+                  src="https://avatars.githubusercontent.com/u/61349443?s=280&v=4"
+                  alt=""
+                />
+              </a>
+              <a
+                href="https://www.facebook.com/people/Luy%E1%BA%BFn-Academy/61560552058905/"
+                target="blank"
+              >
+                <img
+                  style={{
+                    width: "50px",
+                  }}
+                  src="https://cdn-icons-png.flaticon.com/512/1384/1384005.png"
+                  alt="facebook"
+                />
+                <img
+                  style={{
+                    width: "50px",
+                  }}
+                  src="https://famfonts.com/wp-content/uploads/facebook-2-wide.png"
+                  alt=""
+                />
+              </a>
+            </Grid2>
+          </Grid2>
         </Box>
       </Box>
-    </div>
+    </Box>
   );
 }
